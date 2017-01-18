a5d178af4ba6b95b129b22def55bf84d1d045f11
Trym Kristiansen <tuxinet32@gmail.com>
2017-01-18 03:30:58 +0100

    Added Environment and World class skeletons for further development of the environment part of the AI.
    Signed-off-by: Trym Kristiansen <tuxinet32@gmail.com>

---
+++ World.py
@@ -0,0 +1,8 @@
+
+class World:
+    width = 0
+    height = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
+from Environment import Environment
+
 
 def main():
     print(sys.version)
+    environment = Environment(0, 0)
 
 if __name__ == "__main__":
     main()
