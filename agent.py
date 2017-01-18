f1196302360134d1d7a195da38963af27c4f0f7a
Anders Milje <syntox32@gmail.com>
2017-01-18 12:54:30 +0100

    Began sketching the VacuumAgent structure

---
+++ agent.py
@@ -0,0 +1,26 @@
+
+
+class VacuumAgent:
+    """
+    Simple reflex agent.
+
+    See more here:  https://i.milje.me/17-01/18aa5e2.png
+    """
+
+    def __init__(self):
+        pass
+
+    def perceive(self, env):
+        """
+        Perceive the environment.
+
+        :param env: Instance of Environment class.
+        :return: An object describing the current state of the perceivable environment.
+        """
+        pass
+
+    def determine_action(self):
+        pass
+
+    def step(self):
+        pass
