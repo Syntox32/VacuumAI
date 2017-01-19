f4ecf89186bcfe65313df765431f1c13cf2a4189
Anders Milje <syntox32@gmail.com>
2017-01-19 09:45:04 +0100

    Added simulation.py
    Contains class meant to contain the performance score, agent and the environment.
    
    Proposal to use this as the top-level class for the AI.

---
+++ simulation.py
@@ -0,0 +1,36 @@
+
+class VacuumSimulation:
+    """
+
+    """
+
+    def __init__(self, **kwargs):
+        """
+
+        :param test:
+        """
+        self.test = kwargs.pop("test", 0)
+
+
+    def register_agent(self):
+        """
+
+        :return: void
+        """
+        pass
+
+
+    def register_world(self):
+        """
+
+        :return: void
+        """
+        pass
+
+
+    def run(self):
+        """
+
+        :return: bool
+        """
+        pass
