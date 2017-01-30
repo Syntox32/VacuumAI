f3fd1693ea29566b2443466feb18ec2c6056bc08
Anders Milje <syntox32@gmail.com>
2017-01-30 23:14:17 +0100

    configured an actual simulation

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from world import Map
 from simulation import VacuumSimulation
 
 def main():
+    v = VacuumSimulation(width=2,
+                         height=1,
+                         do_steps=10)
 
-    v = VacuumSimulation(width=2, height=1, do_steps=10)
+    # Configuration 1
+    v.reset() # For good measure
+    v.spawn_agent(0, 0)
     results = v.run()
-
+    print(results)
 
 
 if __name__ == "__main__":
