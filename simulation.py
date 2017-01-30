4db3f6b330401f3b3575b808d308408165e09425
Anders Milje <syntox32@gmail.com>
2017-01-30 22:41:25 +0100

    simulation now has some functionality
    records cells_cleaned
    reacts to percepts from the agent
    can spawn the agent and reset itself
    run for N steps

--- simulation.py
+++ simulation.py
@@ -1,36 +1,52 @@
+from world import World
+from agent import Agent
 
 class VacuumSimulation:
     """
-
     """
 
     def __init__(self, **kwargs):
         """
-
         :param test:
         """
-        self.test = kwargs.pop("test", 0)
+        self.width = kwargs.pop("width", 1)
+        self.height = kwargs.pop("height", 2)
+        self.do_steps = kwargs.pop("steps", 10)
 
+        self.world = World(self.width, self.height)
+        self.agent = Agent(self.world)
 
-    def register_agent(self):
-        """
+        self.cells_cleaned = 0
+        self.steps_taken = 0
+        self.agent_score = 0
 
-        :return: void
-        """
-        pass
+    def reset(self):
+        self.cells_cleaned = 0
+        self.steps_taken = 0
+        self.agent_score = 0
 
-
-    def register_world(self):
-        """
-
-        :return: void
-        """
-        pass
-
+    def spawn_agent(self, x=0, y=0):
+        self.agent.set_pos(x, y)
 
     def run(self):
         """
-
-        :return: bool
+        :return: A dictionary with the simulation parameters
         """
-        pass
+        for i in range(self.do_steps):
+            self.agent.perceive(self.world.get_cell_for(self.agent))
+            action = self.agent.step()
+            if action == Agent.CLEAN:
+                self.world.clean_current_cell()
+                self.cells_cleaned += 1
+            elif action == Agent.MOVE_L:
+                pass
+            elif action == Agent.MOVE_R:
+                pass
+            elif action == Agent.NOOP:
+                pass
+
+        return {
+            "cells_cleaned": self.cells_cleaned,
+            "steps_taken": self.steps_taken,
+            "do_steps": self.do_steps
+        }
